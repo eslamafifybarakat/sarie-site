960e96dc64e25c4f1fe4fa7eb82ab8239cc378ab
eslamfront55 <eslamfront55@gmail.com>
2024-01-23 16:57:17 +0200

    add loading overlay + create reset password

--- js/main.js
+++ js/main.js
@@ -1,13 +1,40 @@
-const navbar = document.getElementById('navbar');
+document.addEventListener("DOMContentLoaded", function () {
+  const navbar = document.getElementById('navbar');
 
-function handleScroll() {
-  const scrollPosition = window.scrollY;
+  function handleScroll() {
+    const scrollPosition = window.scrollY;
 
-  if (scrollPosition > 50) {
-    navbar.classList.add('scrolled');
-  } else {
-    navbar.classList.remove('scrolled');
+    if (scrollPosition > 50) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
   }
-}
 
-window.addEventListener('scroll', handleScroll);
+  window.addEventListener('scroll', handleScroll);
+
+  // ===============LOGIN====================
+  document.getElementById('login').addEventListener('click', function () {
+    document.getElementById('loading-overlay').classList.remove('d-none');
+    setTimeout(() => {
+      document.getElementById('success').classList.remove('d-none');
+      document.getElementById('proceed').classList.add('d-none');
+    }, 2500);
+    setTimeout(function () {
+      document.getElementById('loading-overlay').classList.add('d-none');
+      window.location.href = 'index.html';
+    }, 4000);
+  });
+});
+
+// ======================
+document.getElementById('register').addEventListener('click', function () {
+  document.getElementById('loading-overlay').classList.remove('d-none');
+  setTimeout(() => {
+    document.getElementById('success').classList.remove('d-none');
+    document.getElementById('proceed').classList.add('d-none');
+  }, 2500);
+  setTimeout(function () {
+    // document.getElementById('loading-overlay').classList.add('d-none');
+  }, 4000);
+});
