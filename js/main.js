9241fa9870392d9a15b5072ced30374e8fa884e1
eslamfront55 <eslamfront55@gmail.com>
2024-01-24 20:52:52 +0200

    register stepper

--- js/main.js
+++ js/main.js
@@ -8,10 +8,10 @@ document.addEventListener("DOMContentLoaded", function () {
   }
   // ===============AUTH=====================
   function showElement(elementId) {
-    document.getElementById(elementId).classList.remove('d-none');
+    document.getElementById(elementId) ? document.getElementById(elementId).classList.remove('d-none') : '';
   }
   function hideElement(elementId) {
-    document.getElementById(elementId).classList.add('d-none');
+    document.getElementById(elementId) ? document.getElementById(elementId).classList.add('d-none') : '';
   }
   function setLoadingOverlay() {
     showElement('loading-overlay');
@@ -62,4 +62,75 @@ document.addEventListener("DOMContentLoaded", function () {
     setLoadingOverlay();
   }) : '';
 
+});
+// ==================
+let currentStep = 0;
+
+function changeStep(step) {
+  // Hide current step content
+  document.getElementById(`step${currentStep + 1}`).style.display = 'none';
+
+  // Update current step
+  currentStep = step;
+
+  // Show new step content
+  document.getElementById(`step${currentStep + 1}`).style.display = 'block';
+
+  // Update progress bar
+  const progressPercentage = (currentStep + 1) * (100 / 8); // Assuming 8 steps
+  setProgress(progressPercentage);
+  // Update step styles
+  updateStepStyles();
+}
+function setProgress(progressPercentage) {
+  const progressBar = document.getElementById('progressBar');
+  const stepNumber = document.getElementById('stepNumber');
+
+  // Update progress bar width and content
+  progressBar.style.width = `${progressPercentage}%`;
+  progressBar.innerText = `${Math.round(progressPercentage)}%`;
+
+  // Update step number
+  stepNumber.innerText = currentStep + 1;
+}
+function updateStepStyles() {
+  const steps = document.querySelectorAll('.step');
+  steps.forEach((step, index) => {
+    if (index <= currentStep) {
+      step.classList.add('active');
+    } else {
+      step.classList.remove('active');
+    }
+  });
+}
+
+changeStep(0);
+
+// ====================SUBMIT=================
+function updateFileName() {
+  var inputFile = document.getElementById("taxIdCertificate");
+  var fileNameSpan = document.getElementById("pdfCertificationFileName");
+  fileNameSpan.textContent = inputFile.files[0].name;
+}
+function updateVideo() {
+  var inputFile = document.getElementById("video");
+  var fileNameSpan = document.getElementById("videoFileName");
+  fileNameSpan.textContent = inputFile.files[0].name;
+}
+
+document.getElementById("requestDataForm").addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevent the default form submission
+
+  // Get the form element by its id
+  var form = event.target;
+
+  // Loop through all form elements
+  for (var i = 0; i < form.elements.length; i++) {
+    var element = form.elements[i];
+
+    // Check if the element has a value (ignore buttons and other non-input elements)
+    if (element.type !== "button" && element.type !== "submit" && element.value !== undefined) {
+      console.log(element.name + ": " + element.value);
+    }
+  }
 });
