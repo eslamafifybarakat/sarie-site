a872cb8f52a29e75663d63c9cdfa87270763cf45
eslamfront55 <eslamfront55@gmail.com>
2024-01-24 11:07:05 +0200

    fixed

--- js/main.js
+++ js/main.js
@@ -1,72 +1,76 @@
-
 document.addEventListener("DOMContentLoaded", function () {
+  // ====================SCROLL==============
   const navbar = document.getElementById('navbar');
+  const userLoggedIn = window.localStorage.getItem('isLogin') === 'true';
+
   function handleScroll() {
-    const scrollPosition = window.scrollY;
-    if (scrollPosition > 30) {
-      navbar.classList.add('scrolled');
-    } else {
-      navbar.classList.remove('scrolled');
-    }
+    navbar.classList.toggle('scrolled', window.scrollY > 30);
   }
+  // ===============AUTH=====================
+  function showElement(elementId) {
+    document.getElementById(elementId).classList.remove('d-none');
+  }
+  function hideElement(elementId) {
+    document.getElementById(elementId).classList.add('d-none');
+  }
+  function setLoadingOverlay() {
+    showElement('loading-overlay');
+    setTimeout(() => {
+      showElement('success');
+      hideElement('proceed');
+    }, 2500);
+  }
+
+  function redirectToIndex() {
+    const indexPage = window.location.href.includes('ar') ? 'index-ar.html' : 'index.html';
+    window.location.href = indexPage;
+  }
+
+  function redirectToResetPassword() {
+    const resetPasswordPage = window.location.href.includes('ar') ? 'reset-password-ar.html' : 'reset-password.html';
+    window.location.href = resetPasswordPage;
+  }
+
+  function loginUser() {
+    setLoadingOverlay();
+    setTimeout(() => {
+      window.localStorage.setItem('isLogin', true);
+      hideElement('loading-overlay');
+      redirectToIndex();
+    }, 4000);
+  }
+
+  function registerUser() {
+    setLoadingOverlay();
+  }
+
+  function forgetPassword() {
+    setLoadingOverlay();
+    setTimeout(() => {
+      hideElement('loading-overlay');
+      redirectToResetPassword();
+    }, 4000);
+  }
+
   window.addEventListener('scroll', handleScroll);
-  // ==============TOGGLE USER INFO AND LOGIN BUTTON=================
-  if (window.localStorage.getItem('isLogin')) {
-    document.getElementById('user-info').classList.remove('d-none');
-    document.getElementById('login-btn').classList.add('d-none');
+  // ======================LOGIN ===================
+  if (userLoggedIn) {
+    showElement('user-info');
+    hideElement('login-btn');
   }
-  // ==================LOGOUT ACTION ==========================
+  // ==============LOGOUT==================
   document.getElementById('logout').addEventListener('click', function () {
     window.localStorage.removeItem('isLogin');
   });
-  // ===============LOGIN ACTION====================
-  document.getElementById('login').addEventListener('click', function () {
-    document.getElementById('loading-overlay').classList.remove('d-none');
-    setTimeout(() => {
-      document.getElementById('success').classList.remove('d-none');
-      document.getElementById('proceed').classList.add('d-none');
-    }, 2500);
-    setTimeout(function () {
-      window.localStorage.setItem('isLogin', true);
-      document.getElementById('loading-overlay').classList.add('d-none');
-      if (window.location.href.includes('ar')) {
-        window.location.href = 'index-ar.html';
-      } else {
-        window.location.href = 'index.html';
-      }
-    }, 4000);
-  });
-  // ===============REGISTER ACTION====================
-  document.getElementById('register').addEventListener('click', function () {
-    document.getElementById('loading-overlay').classList.remove('d-none');
-    setTimeout(() => {
-      document.getElementById('success').classList.remove('d-none');
-      document.getElementById('proceed').classList.add('d-none');
-    }, 2500);
-  });
-  // ===============FORGET PASSWORD ACTION====================
-  document.getElementById('forget-password').addEventListener('click', function () {
-    document.getElementById('loading-overlay').classList.remove('d-none');
-    setTimeout(() => {
-      document.getElementById('success').classList.remove('d-none');
-      document.getElementById('proceed').classList.add('d-none');
-    }, 2500);
-    setTimeout(function () {
-      document.getElementById('loading-overlay').classList.add('d-none');
-      if (window.location.href.includes('ar')) {
-        window.location.href = 'reset-password-ar.html';
-      } else {
-        window.location.href = 'reset-password.html';
-      }
-    }, 4000);
-  });
-  // ===============RESET PASSWORD ACTION====================
-  document.getElementById('reset-password').addEventListener('click', function () {
-    document.getElementById('loading-overlay').classList.remove('d-none');
-    setTimeout(() => {
-      document.getElementById('success').classList.remove('d-none');
-      document.getElementById('proceed').classList.add('d-none');
-    }, 2500);
-  });
+  // ===================AUTH ACTIONS=============
+  document.getElementById('login') ? document.getElementById('login').addEventListener('click', loginUser) : '';
+
+  document.getElementById('register') ? document.getElementById('register').addEventListener('click', registerUser) : '';
+
+  document.getElementById('forget-password') ? document.getElementById('forget-password').addEventListener('click', forgetPassword) : '';
+
+  document.getElementById('reset-password') ? document.getElementById('reset-password').addEventListener('click', function () {
+    setLoadingOverlay();
+  }) : '';
 
 });
